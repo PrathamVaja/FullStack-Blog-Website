572a1c73ad603ae4077ae8dc616c0cdd244d7e03
Pratham Vaja <prathamvaja8@gmail.com>
2025-05-30 18:12:50 +0530

    mongo url changes

--- Backend/db/mongodb.js
+++ Backend/db/mongodb.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const mongodb_connection =  async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/Blog");
+        await mongoose.connect(
+          "mongodb+srv://pratham:Vaja1234@cluster0.a9ji1gh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        );
         console.log('mongodb connected')
     } catch (error) {
         console.log(error)
