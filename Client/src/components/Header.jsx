29a355927163fcd1139b2676b0e8f88090283538
Pratham Vaja <prathamvaja8@gmail.com>
2025-05-31 13:52:51 +0530

    update favicon and title

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -61,6 +61,7 @@ const Header = () => {
           <span className="text-xl sm:text-3xl font-bold bg-gradient-to-r from-indigo-600 to-purple-600 bg-clip-text text-transparent">
             BloggerBhai
           </span>
+
         </Link>
 
    
